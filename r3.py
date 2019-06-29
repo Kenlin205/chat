e14175bcf7290024c34d9fd6793bfdac0872a254
Kenlin <kenlin205@gmail.com>
2019-06-29 15:32:49 +0800

    r3

---
+++ r3.py
@@ -0,0 +1,14 @@
+import os # operating system
+lines = []
+with open("3.txt", "r",encoding = "utf-8-sig") as f:
+    for line in f:
+        lines.append(line.strip())
+
+for line in lines:
+    s = line.split(" ")
+    time = s[0][:5]
+    name = s[0][5:]
+    print(name)
+
+
+  
